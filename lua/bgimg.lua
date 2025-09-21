58e66051077dfd59c7d29921fd3ab2e386c23edb
kamtos <kamtos@gmail.com>
2025-09-21 14:37:35 +0900

    Feature: .lua/bgimg.lua, add hello_name, hello_nick functions.

--- lua/bgimg.lua
+++ lua/bgimg.lua
@@ -6,6 +6,11 @@ local module = require("bgimg.module")
 ---@field opt string Your config option
 local config = {
   opt = "Hello!",
+
+  -- TEST
+  message = "hello git world from lua script.",
+  first_name = "yamada"
+  nick_name = "gitman"
 }
 
 ---@class MyModule
@@ -19,12 +24,23 @@ M.config = config
 -- you can also put some validation here for those.
 M.setup = function(args)
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
+  -- TEST
+  print(M.config.message)
+  vim.api.nvim_create_user_command('HelloName', M.hello_name, {})
+  vim.api.nvim_create_user_command('HelloNick', M.hello_nick, {})
 end
 
 M.hello = function()
   return module.my_first_function(M.config.opt)
 end
 
-print("hello git world.")
+-- print("hello git world.")
+M.hello_name = function()
+	print(M.config.message .. M.config.first_name)
+end
+
+M.hello_nick = function()
+	print(M.config.message .. M.config.nick_name)
+end
 
 return M
