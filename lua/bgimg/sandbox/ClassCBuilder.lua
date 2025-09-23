85029247c28707427b3b8019450e1f6a4f18a24b
kamtos <kamtos@gmail.com>
2025-09-23 16:11:36 +0900

    Add: BuilderTEmplate. ClassCBuilder, ClassC_spec.

---
+++ lua/bgimg/sandbox/ClassCBuilder.lua
@@ -0,0 +1,65 @@
+-- local t = require("template")
+-- template has member field.
+local Object = require("bgimg.sandbox.ClassC")
+
+local logging = require("logging")
+local ansicolors = require("ansicolors")
+require("logging.file")
+-- init_spec.lua
+local logger = logging.file({
+  filename = "log/test_ClassCBuilder_%s.log",
+  datePattern = "%Y-%m-%d",
+})
+
+function Now()
+  datetime = os.date("%m")
+    .. "/"
+    .. os.date("%d")
+    .. " "
+    .. os.date("%H")
+    .. ":"
+    .. os.date("%M")
+    .. ":"
+    .. os.date("%I")
+  return datetime
+end
+
+-- Class: TemplateBuilder
+local M = {}
+
+M = {
+  new = function()
+    -- new = function(member)
+    local obj = {
+      --[[
+	member = "",
+	]]
+      content = "",
+    }
+    return obj
+  end,
+
+  of_default = function()
+    local obj = Object:prototype(M)
+    return obj
+  end,
+}
+
+function M:hello()
+  -- print("hello "..self.member..".")
+  print("hello " .. self.member .. ".")
+end
+
+function M:build()
+  local template = Template.new(self.member)
+  return template
+end
+
+function M:with_content(str)
+  logger:info(Now() .. " builder.with_member " .. "'" .. str .. "'")
+  self.member = member
+  return self
+end
+
+-- Class: BuilderTemplate
+return M
--- tests/bgimg/ClassC_spec.lua
+++ tests/bgimg/ClassC_spec.lua
@@ -1,30 +1,29 @@
-local ClassC = require("bgimg.ClassC")
-local P = require("bgimg.P")
+local ClassC = require("bgimg.sandbox.ClassC")
+local P = require("bgimg.util.P")
 local colors = require("ansicolors")
 local logging = require("logging")
 require("logging.file")
 -- init_spec.lua
 local logger = logging.file({
-	filename = "log/test_ClassC_%s.log",
-	datePattern = "%Y-%m-%d",
+  filename = "log/test_ClassC_%s.log",
+  datePattern = "%Y-%m-%d",
 })
 
-logger:info("Stringx test")
+logger:info("ClassC test")
 
-describe("Stringx test", function()
-	describe("trim", function()
-		it("' abc ' length is 3", function()
-			local c = ClassC.new(" abc ")
-			logger:info("strx: " .. c:to_string())
-			-- logger:info("strx: " .. strx.content)
-			assert.is_truthy(0)
-		end)
+describe("to_string test", function()
+  describe("to_string", function()
+    it("'abc' to_string 'abc'", function()
+      str1 = "abc"
+      local c = ClassC.new(str1)
+      str2 = c:to_string()
 
-		it("'1' equal '0'", function()
-			assert.is_equal(1, 0)
-		end)
-	end)
+      logger:info("content: " .. c:to_string())
+      -- logger:info("strx: " .. strx.content)
+      assert.is_equal(str1, str2)
+    end)
+  end)
 end)
 
-P(colors("%{red}ClassC%{reset} test."))
+-- P(colors("%{red}ClassC%{reset} test."))
 -- P("package.path: " .. package.path)
